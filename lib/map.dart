059da50e0e20c968c6381edd3199f3395d7c1461
orothy579 <108617889+orothy579@users.noreply.github.com>
2022-11-27 17:28:08 +0900

    Drawer and app bar

--- lib/map.dart
+++ lib/map.dart
@@ -83,9 +83,61 @@ class _MapPageState extends State<MapPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: Colors.purple,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.all(13.0),
+                      child: Text(
+                        'Spray',
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ListTile(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 30.0),
+                leading:  Icon(Icons.church, color: Colors.purple.shade100),
+                title: const Text('Favorite Group'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/hotel');
+                },
+              ),
+              ListTile(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 30.0),
+                leading:  Icon(Icons.person, color: Colors.purple.shade100),
+                title: const Text('My Profile'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/my');
+                },
+              ),
+              ListTile(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 30.0),
+                leading:  Icon(Icons.logout, color: Colors.purple.shade100),
+                title: const Text('Log Out'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/login');
+                },
+              ),
+            ],
+          ),
+        ),
         appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
+          backgroundColor: Colors.purple,
+          title: const Text("Map"), centerTitle: true,
+
+
         ),
         body:
         PageView(
