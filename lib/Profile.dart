b0322d68a6aaed3fb0a042181f24b13c1c493e87
Y-eseong <21900149@handong.ac.kr>
2022-12-05 16:03:59 +0900

    profile & calendar

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 
 import 'model/user.dart';
 
-
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
 
@@ -17,66 +16,117 @@ class _ProfileState extends State<Profile> {
   final _uid = FirebaseAuth.instance.currentUser!.uid;
 
   Future<Users> getUser(String userkey) async {
-    DocumentReference<Map<String,dynamic>> documentReference =
+    DocumentReference<Map<String, dynamic>> documentReference =
         FirebaseFirestore.instance.collection('users').doc(userkey);
-    final DocumentSnapshot<Map<String,dynamic>> documentSnapshot =
+    final DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
         await documentReference.get();
     Users user = Users.fromSnapshot(documentSnapshot);
     return user;
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple.shade100,
       appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text("Profile"),
+        centerTitle: true,
         actions: <Widget>[
           IconButton(
               icon: const Icon(
                 Icons.exit_to_app,
               ),
-              onPressed: () async  {
+              onPressed: () async {
                 Navigator.pushNamed(context, '/login');
                 await FirebaseAuth.instance.signOut();
-  }),
+              }),
         ],
       ),
       body: FutureBuilder<Users>(
         future: getUser(_uid),
-        builder: (context, snapshot){
-          if(snapshot.hasData){
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
             Users data = snapshot.data!;
             return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                const SizedBox(height: 30),
                 Container(
-                  child: Image.network(data.image!),
+                  width: 300,
+                  height: 250,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Image.network(
+                      data.image!,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
                 ),
-                Text("UID: ${data.uid!}"),
-                Text("Email: ${data.email!}"),
-                Text("Chanhwi Lee"),
-                SizedBox(height: 8.0,),
-                Text("I promise to take the test honestly before GOD")
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                    child: Column(children: [
+                      const Divider(
+                        height: 40,
+                        thickness: 1,
+                        color: Colors.deepPurple,
+                      ),
+                      const SizedBox(height: 50),
+                      const Text(
+                        "Chanhwi Lee",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      Text(
+                        "Email: ${data.email!}",
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      Text("Pray Count: ${data.praynumber}",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          )),
+                      const SizedBox(height: 30),
+                      Text("UID: ${data.uid!}",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          )),
+                      const SizedBox(height: 50),
+                      Text(
+                        "    ${data.status_message!}",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                      )
+                    ]))
               ],
-
             );
-          }
-          else if(snapshot.hasError){
+          } else if (snapshot.hasError) {
             return Padding(
                 padding: const EdgeInsets.all(8.0),
-                child : Text(
+                child: Text(
                   'Error: ${snapshot.error}',
                   style: TextStyle(fontSize: 15),
-                )
-            );
-          }
-
-          else{
+                ));
+          } else {
             return const Center(child: CircularProgressIndicator());
           }
         },
       ),
-
-
     );
   }
 }
---
+++ lib/calendar.dart
@@ -0,0 +1,161 @@
+import 'package:bottom_bar/bottom_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced_calendar/flutter_advanced_calendar.dart';
+import 'package:spray/login.dart';
+import 'package:spray/map.dart';
+import 'package:spray/timer.dart';
+import 'package:spray/board.dart';
+import 'package:spray/home.dart';
+
+import 'model/user.dart';
+
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({Key? key}) : super(key: key);
+
+  @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  Future<Users> getUser(String userkey) async {
+    DocumentReference<Map<String,dynamic>> documentReference =
+    FirebaseFirestore.instance.collection('users').doc(userkey);
+    final DocumentSnapshot<Map<String,dynamic>> documentSnapshot =
+    await documentReference.get();
+    Users user = Users.fromSnapshot(documentSnapshot);
+    return user;
+  }
+
+  int _currentPage = 3;
+
+  final _children = [
+    TimerPage(),
+    BoardPage(),
+    HomePage(),
+    CalendarPage(), // Calender Page
+    MapPage(),
+  ];
+
+  _onTap() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) => _children[_currentPage])); // this has changed
+  }
+
+  final _pageController = PageController();
+  final _calendarControllerToday = AdvancedCalendarController.today();
+  final _calendarControllerCustom =
+  AdvancedCalendarController.custom(DateTime.now());
+  //final ddd = DateTime.parse('${DateTime.fromMillisecondsSinceEpoch(Users.time)}');
+  final List<DateTime> events = [
+    DateTime.now(),
+    DateTime(2022, 12, 1),
+    DateTime(2022, 12, 2),
+    DateTime(2022, 12, 5),
+  ];
+
+    // DateTiem(ddd.year, ddd.month, ddd.day),
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: const Text("Calendar"), centerTitle: true,
+        ),
+        body:
+        PageView(
+          controller: _pageController,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // AdvancedCalendar(
+                //   controller: _calendarControllerToday,
+                //   events: events,
+                //   startWeekDay: 1,
+                // ),
+                Theme(
+                  data: ThemeData.light().copyWith(
+                    textTheme: ThemeData.light().textTheme.copyWith(
+                      subtitle1: ThemeData.light().textTheme.subtitle1?.copyWith(
+                        fontSize: 16,
+                        color: theme.colorScheme.secondary,
+                      ),
+                      bodyText1: ThemeData.light().textTheme.bodyText1?.copyWith(
+                        fontSize: 14,
+                        color: Colors.black54,
+                      ),
+                      bodyText2: ThemeData.light().textTheme.bodyText1?.copyWith(
+                        fontSize: 12,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    primaryColor: Colors.purple,
+                    highlightColor: Colors.purple.shade100,
+                    disabledColor: Colors.green,
+                  ),
+                  child: AdvancedCalendar(
+                    controller: _calendarControllerCustom,
+                    events: events,
+                    weekLineHeight: 48.0,
+                    startWeekDay: 1,
+                    innerDot: true,
+                    keepLineSize: true,
+                    calendarTextStyle: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      height: 1.3125,
+                      letterSpacing: 0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ]
+        ),
+
+        bottomNavigationBar: BottomBar(
+          selectedIndex: _currentPage,
+          onTap: (int index) {
+            _pageController.jumpToPage(index);
+            setState(() => _currentPage = index);
+            _onTap();
+          },
+          items: <BottomBarItem>[
+            BottomBarItem(
+                icon: Icon(Icons.timer),
+                title: Text('Timer'),
+                activeColor: Colors.purple
+            ),
+            BottomBarItem(
+                icon: Icon(Icons.group_add),
+                title: Text('Group'),
+                activeColor: Colors.purple
+            ),
+            BottomBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Home'),
+                activeColor: Colors.purple
+            ),
+            BottomBarItem(
+                icon: Icon(Icons.calendar_month),
+                title: Text('calendar'),
+                activeColor: Colors.purple
+            ),
+            BottomBarItem(
+                icon: Icon(Icons.map),
+                title: Text('Map'),
+                activeColor: Colors.purple
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
